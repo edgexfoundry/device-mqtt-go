c3a540571b2987e6a2bfb18fb2dfafcde0b14f19
Bruce <weichou1229@gmail.com>
2019-03-11 16:50:36 +0800

    Updated to the replacement of Device.Addressable with Device.Protocols, and the deprecation of Schedule* objects by device-sdk-go.
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/edgexfoundry/device-mqtt-go
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/eclipse/paho.mqtt.golang v1.1.1
-	github.com/edgexfoundry/device-sdk-go v0.0.0-20190306090327-0f835be981b7
-	github.com/edgexfoundry/go-mod-core-contracts v0.0.0-20190222195937-d11dc767d321
+	github.com/edgexfoundry/device-sdk-go v0.0.0-20190316170723-8e7a6966fd13
+	github.com/edgexfoundry/go-mod-core-contracts v0.0.0-20190315142420-7a2734aa6c01
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/mux v1.7.0 // indirect
--- internal/driver/config.go
+++ internal/driver/config.go
@@ -1,6 +1,6 @@
 // -*- Mode: Go; indent-tabs-mode: t -*-
 //
-// Copyright (C) 2018 IOTech Ltd
+// Copyright (C) 2019 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 
 	"github.com/BurntSushi/toml"
+	"github.com/edgexfoundry/go-mod-core-contracts/models"
 )
 
 type configuration struct {
@@ -28,6 +29,16 @@ type SubscribeInfo struct {
 	KeepAlive    int
 	MqttClientId string
 	Topic        string
+}
+
+type ConnectionInfo struct {
+	Schema   string
+	Host     string
+	Port     string
+	User     string
+	Password string
+	ClientId string
+	Topic    string
 }
 
 // LoadConfigFromFile use to load toml configuration
@@ -55,4 +66,47 @@ func LoadConfigFromFile() (*configuration, error) {
 		return config, fmt.Errorf("unable to parse configuration file (%s): %v", filePath, err.Error())
 	}
 	return config, err
+}
+
+func CreateConnectionInfo(protocols map[string]models.ProtocolProperties) (info ConnectionInfo, err error) {
+	errorMessage := "unable to create connection info, protocol config '%s' not exist"
+	protocol, ok := protocols[Protocol]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Protocol)
+	}
+	schema, ok := protocol[Schema]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Schema)
+	}
+	host, ok := protocol[Host]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Host)
+	}
+	port, ok := protocol[Port]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Port)
+	}
+	user, ok := protocol[User]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, User)
+	}
+	password, ok := protocol[Password]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Password)
+	}
+	clientId, ok := protocol[ClientId]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, ClientId)
+	}
+	topic, ok := protocol[Topic]
+	if !ok {
+		return info, fmt.Errorf(errorMessage, Topic)
+	}
+
+	info = ConnectionInfo{
+		Schema: schema, Host: host, Port: port,
+		User: user, Password: password,
+		ClientId: clientId, Topic: topic,
+	}
+	return info, nil
 }
