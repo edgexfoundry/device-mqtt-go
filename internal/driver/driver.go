682662e6edc854f4c95c4a6cdf63686692a08544
weichou <weichou1229@gmail.com>
2019-10-18 16:07:43 +0800

    Update Device Service to Latest Version of Device SDK
    Signed-off-by: weichou <weichou1229@gmail.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -446,3 +446,18 @@ func (d *Driver) fetchCommandResponse(cmdUuid string) (string, bool) {
 
 	return fmt.Sprintf("%v", cmdResponse), ok
 }
+
+func (d *Driver) AddDevice(deviceName string, protocols map[string]models.ProtocolProperties, adminState models.AdminState) error {
+	d.Logger.Debug(fmt.Sprintf("Device %s is added", deviceName))
+	return nil
+}
+
+func (d *Driver) UpdateDevice(deviceName string, protocols map[string]models.ProtocolProperties, adminState models.AdminState) error {
+	d.Logger.Debug(fmt.Sprintf("Device %s is updated", deviceName))
+	return nil
+}
+
+func (d *Driver) RemoveDevice(deviceName string, protocols map[string]models.ProtocolProperties) error {
+	d.Logger.Debug(fmt.Sprintf("Device %s is removed", deviceName))
+	return nil
+}
