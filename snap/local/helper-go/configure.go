18bdc01fcc8ed26343b4b5335b146dcc0f8f5df0
Mengyi Wang <mengyi.wang@canonical.com>
2022-06-29 17:11:45 +0200

    refactor(snap): edgex-snap-hooks related upgrade
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/helper-go/configure.go
+++ snap/local/helper-go/configure.go
@@ -20,56 +20,82 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	hooks "github.com/canonical/edgex-snap-hooks/v2"
 	"github.com/canonical/edgex-snap-hooks/v2/log"
 	"github.com/canonical/edgex-snap-hooks/v2/options"
-	local "github.com/edgexfoundry/device-mqtt-go/hooks"
 )
+
+// ConfToEnv defines mappings from snap config keys to EdgeX environment variable
+// names that are used to override individual device-mqtt's [Driver]  configuration
+// values via a .env file read by the snap service wrapper.
+//
+// The syntax to set a configuration key is:
+//
+// env.<section>.<keyname>
+//
+var ConfToEnv = map[string]string{
+	// [MQTTBrokerInfo]
+	"mqttbrokerinfo.schema":     "MQTTBROKERINFO_SCHEMA",
+	"mqttbrokerinfo.host":       "MQTTBROKERINFO_HOST",
+	"mqttbrokerinfo.port":       "MQTTBROKERINFO_PORT",
+	"mqttbrokerinfo.qos":        "MQTTBROKERINFO_QOS",
+	"mqttbrokerinfo.keep-alive": "MQTTBROKERINFO_KEEPALIVE",
+	"mqttbrokerinfo.client-id":  "MQTTBROKERINFO_CLIENTID",
+
+	"mqttbrokerinfo.credentials-retry-time":  "MQTTBROKERINFO_CREDENTIALSRETRYTIME",
+	"mqttbrokerinfo.credentials-retry-wait":  "MQTTBROKERINFO_CREDENTIALSRETRYWAIT",
+	"mqttbrokerinfo.conn-establishing-retry": "MQTTBROKERINFO_CONNESTABLISHINGRETRY",
+	"mqttbrokerinfo.conn-retry-wait-time":    "MQTTBROKERINFO_CONNRETRYWAITTIME",
+
+	"mqttbrokerinfo.auth-mode":        "MQTTBROKERINFO_AUTHMODE",
+	"mqttbrokerinfo.credentials-path": "MQTTBROKERINFO_CREDENTIALSPATH",
+
+	"mqttbrokerinfo.incoming-topic": "MQTTBROKERINFO_INCOMINGTOPIC",
+	"mqttbrokerinfo.response-topic": "MQTTBROKERINFO_RESPONSETOPIC",
+
+	// [Device]
+	"device.update-last-connected": "DEVICE_UPDATELASTCONNECTED",
+	"device.use-message-bus":       "DEVICE_USEMESSAGEBUS",
+}
 
 var cli *hooks.CtlCli = hooks.NewSnapCtl()
 
-func main() {
+// configure is called by the main function
+func configure() {
 	var debug = false
 	var err error
 	var envJSON string
 
 	status, err := cli.Config("debug")
 	if err != nil {
-		fmt.Println(fmt.Sprintf("edgex-device-mqtt:configure: can't read value of 'debug': %v", err))
-		os.Exit(1)
+		log.Fatalf("edgex-device-mqtt:configure: can't read value of 'debug': %v", err)
 	}
 	if status == "true" {
 		debug = true
 	}
 
 	if err = hooks.Init(debug, "edgex-device-mqtt"); err != nil {
-		fmt.Println(fmt.Sprintf("edgex-device-mqtt:configure: initialization failure: %v", err))
-		os.Exit(1)
-
+		log.Fatalf("edgex-device-mqtt:configure: initialization failure: %v", err)
 	}
 
 	log.SetComponentName("configure")
 	if err := options.ProcessAppConfig("device-mqtt"); err != nil {
-		hooks.Error(fmt.Sprintf("could not process options: %v", err))
-		os.Exit(1)
+		log.Fatalf("could not process options: %v", err)
 	}
 
 	// read env var override configuration
 	envJSON, err = cli.Config(hooks.EnvConfig)
 	if err != nil {
-		hooks.Error(fmt.Sprintf("Reading config 'env' failed: %v", err))
-		os.Exit(1)
+		log.Fatalf("Reading config 'env' failed: %v", err)
 	}
 
 	if envJSON != "" {
 		hooks.Debug(fmt.Sprintf("edgex-device-mqtt:configure: envJSON: %s", envJSON))
-		err = hooks.HandleEdgeXConfig("device-mqtt", envJSON, local.ConfToEnv)
+		err = hooks.HandleEdgeXConfig("device-mqtt", envJSON, ConfToEnv)
 		if err != nil {
-			hooks.Error(fmt.Sprintf("HandleEdgeXConfig failed: %v", err))
-			os.Exit(1)
+			log.Fatalf("HandleEdgeXConfig failed: %v", err)
 		}
 	}
 
@@ -78,8 +104,7 @@ func main() {
 	// are provided by default.
 	autostart, err := cli.Config(hooks.AutostartConfig)
 	if err != nil {
-		hooks.Error(fmt.Sprintf("Reading config 'autostart' failed: %v", err))
-		os.Exit(1)
+		log.Fatalf("Reading config 'autostart' failed: %v", err)
 	}
 	if autostart == "" {
 		hooks.Debug("edgex-device-mqtt autostart is NOT set, initializing to 'no'")
@@ -96,15 +121,13 @@ func main() {
 	case "yes":
 		err = cli.Start("device-mqtt", true)
 		if err != nil {
-			hooks.Error(fmt.Sprintf("Can't start service - %v", err))
-			os.Exit(1)
+			log.Fatalf("Can't start service - %v", err)
 		}
 	case "false":
 		// no action necessary
 	case "no":
 		// no action necessary
 	default:
-		hooks.Error(fmt.Sprintf("Invalid value for 'autostart' : %s", autostart))
-		os.Exit(1)
+		log.Fatalf("Invalid value for 'autostart' : %s", autostart)
 	}
 }
---
+++ snap/local/helper-go/go.mod
@@ -0,0 +1,5 @@
+module github.com/edgexfoundry/device-mqtt-go/hooks
+
+require github.com/canonical/edgex-snap-hooks/v2 v2.3.0
+
+go 1.17
