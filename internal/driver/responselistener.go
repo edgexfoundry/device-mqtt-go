387f503e80cf00ad0e4df478706022a6b802b76c
Marc-Philippe Fuller <marc-philippe.fuller@intel.com>
2023-04-19 17:57:58 -0700

    refactor!: Make multi-level topics the only implementation
    BREAKING CHANGE: Removed single level topics, multi level is used by default
    
    closes: #551
    
    Signed-off-by: Marc-Philippe Fuller <marc-philippe.fuller@intel.com>

--- internal/driver/responselistener.go
+++ internal/driver/responselistener.go
@@ -7,7 +7,6 @@
 package driver
 
 import (
-	"encoding/json"
 	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -16,31 +15,16 @@ import (
 func (d *Driver) onCommandResponseReceived(client mqtt.Client, message mqtt.Message) {
 	var uuid string
 
-	if d.serviceConfig.MQTTBrokerInfo.UseTopicLevels {
-		topic := message.Topic()
-		metaData := strings.Split(topic, "/")
+	topic := message.Topic()
+	metaData := strings.Split(topic, "/")
 
-		if len(metaData) == 0 {
-			driver.Logger.Errorf("[Response listener] Command response ignored. metaData in the message is not sufficient to retrieve UUID: topic=%v msg=%v", message.Topic(), metaData)
-			return
-		} else {
-			uuid = metaData[len(metaData)-1]
-		}
+	if len(metaData) == 0 {
+		driver.Logger.Errorf("[Response listener] Command response ignored. metaData in the message is not sufficient to retrieve UUID: topic=%v msg=%v", message.Topic(), metaData)
+		return
 	} else {
-		var response map[string]interface{}
-		var ok bool
-
-		err := json.Unmarshal(message.Payload(), &response)
-		if err != nil {
-			driver.Logger.Errorf("Error unmarshaling payload: %s", err)
-			return
-		}
-		uuid, ok = response["uuid"].(string)
-		if !ok {
-			driver.Logger.Errorf("[Response listener] Command response ignored. No UUID found in the message: topic=%v msg=%v", message.Topic(), string(message.Payload()))
-			return
-		}
+		uuid = metaData[len(metaData)-1]
 	}
+
 	driver.CommandResponses.Store(uuid, string(message.Payload()))
 	driver.Logger.Debugf("[Response listener] Command response received: topic=%v uuid=%v msg=%v", message.Topic(), uuid, string(message.Payload()))
 }
