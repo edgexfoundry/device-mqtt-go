294c6def2b0b795a6d478a2cc5574578b861ffe8
Leonard Goodell <leonard.goodell@intel.com>
2023-05-01 11:46:26 -0700

    feat!: Handle receiving multiple readings in the async payload
    BREAKING CHANGE: The published async payload must now be json containing the resource names and values, even when a single resource is sent. Redendent single resource Device Commands (`testrandfloat32`, `testrandfloat64`, `testping` and `testmessage`) in `mqtt.test.device.profile.yml` have be removed. Use the corrisponding resource name (`randfloat32`, `randfloat64`, `ping` or `message`) instead.
    
    closes #299
    
    Signed-off-by: Leonard Goodell <leonard.goodell@intel.com>

--- internal/driver/incominglistener.go
+++ internal/driver/incominglistener.go
@@ -7,55 +7,92 @@
 package driver
 
 import (
+	"encoding/json"
 	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/edgexfoundry/device-sdk-go/v3/pkg/models"
+	sdkModels "github.com/edgexfoundry/device-sdk-go/v3/pkg/models"
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/models"
 )
 
-const (
-	name = "name"
-	cmd  = "cmd"
-)
-
-func (d *Driver) onIncomingDataReceived(client mqtt.Client, message mqtt.Message) {
+func (d *Driver) onIncomingDataReceived(_ mqtt.Client, message mqtt.Message) {
 	var deviceName string
-	var resourceName string
-	var reading interface{}
+	var sourceName string
 
+	var deviceResources []models.DeviceResource
 	incomingTopic := message.Topic()
 	subscribedTopic := d.serviceConfig.MQTTBrokerInfo.IncomingTopic
 	subscribedTopic = strings.Replace(subscribedTopic, "#", "", -1)
 	incomingTopic = strings.Replace(incomingTopic, subscribedTopic, "", -1)
 	metaData := strings.Split(incomingTopic, "/")
 	if len(metaData) != 2 {
-		driver.Logger.Errorf("[Incoming listener] Incoming reading ignored, incoming topic data should have format .../<device_name>/<resource_name>: `%s`", incomingTopic)
+		driver.Logger.Errorf("[Incoming listener] Incoming data ignored, incoming topic data should have format .../<device_name>/<source_name>: `%s`", incomingTopic)
 		return
 	}
 	deviceName = metaData[0]
-	resourceName = metaData[1]
-	reading = string(message.Payload())
+	sourceName = metaData[1]
 
-	deviceObject, ok := d.sdk.DeviceResource(deviceName, resourceName)
+	deviceCommand, ok := d.sdk.DeviceCommand(deviceName, sourceName)
 	if !ok {
-		driver.Logger.Errorf("[Incoming listener] Incoming reading ignored, device resource `%s` not found from the device `%s`", resourceName, deviceName)
-		return
+		deviceResource, ok := d.sdk.DeviceResource(deviceName, sourceName)
+		if !ok {
+			driver.Logger.Errorf("[Incoming listener] Incoming data ignored, source name `%s` not found as Device Command or Device Resource on the device `%s`", sourceName, deviceName)
+			return
+		}
+
+		if !strings.Contains(strings.ToUpper(deviceResource.Properties.ReadWrite), "R") {
+			driver.Logger.Errorf("[Incoming listener] Incoming data ignored, Device Resource `%s` not Readable", sourceName)
+			return
+		}
+
+		deviceResources = append(deviceResources, deviceResource)
+	} else {
+		if !strings.Contains(strings.ToUpper(deviceCommand.ReadWrite), "R") {
+			driver.Logger.Errorf("[Incoming listener] Incoming data ignored, Device Command `%s` not Readable", sourceName)
+			return
+		}
+
+		for _, resourceOperation := range deviceCommand.ResourceOperations {
+			deviceResource, ok := d.sdk.DeviceResource(deviceName, resourceOperation.DeviceResource)
+			if !ok {
+				driver.Logger.Errorf("[Incoming listener] Incoming data ignored, resource name `%s` from Device Command %s not found on the device `%s`", resourceOperation.DeviceResource, sourceName, deviceName)
+				return
+			}
+
+			deviceResources = append(deviceResources, deviceResource)
+		}
 	}
 
-	req := models.CommandRequest{
-		DeviceResourceName: resourceName,
-		Type:               deviceObject.Properties.ValueType,
-	}
-
-	result, err := newResult(req, reading)
+	var asyncData map[string]interface{}
+	err := json.Unmarshal(message.Payload(), &asyncData)
 	if err != nil {
-		driver.Logger.Errorf("[Incoming listener] Incoming reading ignored, %v", err)
+		driver.Logger.Errorf("[Incoming listener] Error un-marshaling incoming data : %v", err)
 		return
 	}
 
-	asyncValues := &models.AsyncValues{
+	var commandValues []*sdkModels.CommandValue
+
+	for _, resource := range deviceResources {
+		asyncValue, ok := asyncData[resource.Name]
+		if !ok {
+			driver.Logger.Errorf("[Incoming listener] Incoming data ignored: Resource Name %s not found in payload (%s)", resource.Name, string(message.Payload()))
+			return
+		}
+
+		commandValue, err := newResult(resource, asyncValue)
+		if err != nil {
+			driver.Logger.Errorf("[Incoming listener] Incoming data ignored: %v", err)
+			return
+		}
+
+		commandValues = append(commandValues, commandValue)
+
+	}
+
+	asyncValues := &sdkModels.AsyncValues{
 		DeviceName:    deviceName,
-		CommandValues: []*models.CommandValue{result},
+		SourceName:    sourceName,
+		CommandValues: commandValues,
 	}
 
 	driver.Logger.Debugf("[Incoming listener] Incoming reading received: topic=%v msg=%v", message.Topic(), string(message.Payload()))
